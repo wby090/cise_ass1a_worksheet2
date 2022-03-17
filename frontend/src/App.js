691c4563aed58443cf5520c044f100f8d167d1fc
wby090 <2905621639@qq.com>
2022-03-17 08:47:19 +0800

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to CISE - the home of learning and fun</h1>
+    </div>
+  );
+}
+
+export default App;
